15169455f3ede5a34059d1228be0ce7040062374
Pablo Barrios <exepablo96@gmail.com>
2025-04-14 19:06:09 -0300

    apply useLazyQuery

--- src/Persons.jsx
+++ src/Persons.jsx
@@ -1,13 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { gql, useLazyQuery } from "@apollo/client";
+
+const FIND_PERSON = gql`
+  query findPersonByName($nameToSearch: String!) {
+    findPerson(name: $nameToSearch) {
+      name
+      phone
+      address {
+        street
+        city
+      }
+    }
+  }
+`;
 
 const Persons = ({ persons }) => {
+  const [getPerson, result] = useLazyQuery(FIND_PERSON);
+  const [person, setPerson] = useState(null);
+  const showPerson = (name) => {
+    getPerson({ variables: { nameToSearch: name } });
+  };
+
+  useEffect(() => {
+    if (result.data) {
+      setPerson(result.data.findPerson);
+    }
+  }, [result]);
+
+  if (person) {
+    return (
+      <div>
+        <h2>{person.name}</h2>
+        <div>{person.address.street}</div>
+        <div>{person.phone}</div>
+        <button onClick={() => setPerson(null)}>close</button>
+      </div>
+    );
+  }
+
+  if (persons === null) return null;
+
   return (
     <div>
       <h2>Persons</h2>
-      {persons.map((p) => {
+      {persons.map((person) => {
         return (
-          <div key={p.id}>
-            {p.name} {p.phone}
+          <div key={person.id} onClick={() => showPerson(person.name)}>
+            {person.name} {person.phone}
           </div>
         );
       })}
