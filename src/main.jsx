836d640a9786e23d868f53b8989fbfe165c2f6bc
Pablo Barrios <exepablo96@gmail.com>
2025-04-17 00:40:14 -0300

    devtools true

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import {
   InMemoryCache,
 } from "@apollo/client";
 const client = new ApolloClient({
+  connectToDevTools: true,
   cache: new InMemoryCache(),
   link: new HttpLink({
     uri: "http://localhost:4000", //esto vendria del .env
